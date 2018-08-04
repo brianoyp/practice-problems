2a8656676e9e5e419858e5b25d90dee71c7b2da0
Brian Oh <brianoyp@ldschurch.org>
2018-08-04 12:54:31 -0600

    code is working...

--- array_rotate/solution/array_rotate.js
+++ array_rotate/solution/array_rotate.js
@@ -1,4 +1,19 @@
 module.exports = function(arr, n){
+
+	// rotate all the way make it same as the begginning. so we can omit that.
+	var num = n % arr.length;
+	var length = arr.length;
+
 	var result = [];
+
+	// push elements to result starting from the index "length-num". 
+	// When it reaches to the end, go to first element and keep pushing and stop right before we started. 
+	var startIndex = length-num;
+	var endIndex = startIndex + length;
+
+	for (var i = startIndex; i < endIndex; i++){
+		result.push(arr[i%length]); 
+	}
+
 	return result;
 };
--- array_rotate/test/array_rotate.test.js
+++ array_rotate/test/array_rotate.test.js
@@ -6,5 +6,11 @@ describe("#arrayRotate", function(){
 	it("rotate n time the array given", function(){
 		expect(arrayRotate([1, 2, 3],1)).to.eql([3, 1, 2]);
 		expect(arrayRotate([1, 2, 3, 4, 5],3)).to.eql([3, 4, 5, 1, 2]);
+		expect(arrayRotate(['a', 'hello', 'apple', 'tree', 0, 1],4)).to.eql(['apple', 'tree', 0, 1, 'a', 'hello']);
+	});
+
+	it("should work even of n is bigger than the length of array", function(){
+		expect(arrayRotate([1, 2, 3, 4, 5],13)).to.eql([ 3, 4, 5, 1, 2]);
+		expect(arrayRotate([1, 2, 3, 4, 5],1000)).to.eql([ 1, 2, 3, 4, 5 ]);
 	});
 });
